72def301430cdc6b7edc7aeb928038074ab68351
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-05-03 03:27:13 +0900

    Bump cask to v0.1.3

--- Casks/agent-sushi.rb
+++ Casks/agent-sushi.rb
@@ -1,6 +1,6 @@
 cask "agent-sushi" do
-  version "0.1.2"
-  sha256 "c09b5b10cbe47e59305ec2dd63ae78b550b48f40ed29a4718dfbaca8ebc9813c"
+  version "0.1.3"
+  sha256 "123db51bf2d1249d967c08f0465f0d659844f452217f87e9832f6db8669ea98c"
 
   url "https://github.com/kmatsushita1012/homebrew-agent-sushi-notifier/releases/download/v#{version}/agent-sushi-#{version}.tar.gz"
   name "Agent Sushi"
