3069cd1ae1517d71a78ba9e8e7a4038aee9c9ba0
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-05-03 13:54:15 +0900

    Bump cask to v0.1.9

--- Casks/agent-sushi.rb
+++ Casks/agent-sushi.rb
@@ -1,6 +1,6 @@
 cask "agent-sushi" do
-  version "0.1.8"
-  sha256 "6c88eea7490d510cee400f960c82143ddcd3c6ad6cacf735ee3d5fdbcb6a88f9"
+  version "0.1.9"
+  sha256 "ef5dc2a1b8c2b3abc29bcfcbc840cdfbf283a4f1eb6cb162b2d3d23bfacf945c"
 
   url "https://github.com/kmatsushita1012/agent-sushi/releases/download/v#{version}/agent-sushi-#{version}.tar.gz"
   name "Agent Sushi"
