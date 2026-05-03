2295ead9f1dfb19fa4d625bd0aed63756f0e1fee
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-05-03 11:35:43 +0900

    fix: update sha256 for flat tarball

--- Casks/agent-sushi.rb
+++ Casks/agent-sushi.rb
@@ -1,6 +1,6 @@
 cask "agent-sushi" do
   version "0.1.8"
-  sha256 "0c39d385d8f2225c24801fddca4fa739220dee88e8a3b642a9e0b4e89a2bb578"
+  sha256 "17f2ecb8cb55d5bc8292c5536a3cb8be62b22321b6d39327f910ddd72b550698"
 
   url "https://github.com/kmatsushita1012/homebrew-agent-sushi/releases/download/v#{version}/agent-sushi-#{version}.tar.gz"
   name "Agent Sushi"
