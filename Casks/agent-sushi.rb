2bcf0882a485b275f7c7809c3780d4b944a5842c
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-05-03 13:00:13 +0900

    fix: disable sandbox to access home directory

--- Casks/agent-sushi.rb
+++ Casks/agent-sushi.rb
@@ -1,6 +1,6 @@
 cask "agent-sushi" do
   version "0.1.8"
-  sha256 "815ae6a94c6f4a2f80733428d74eb3400759f04ae955545a0b47ed5c525cf62c"
+  sha256 "05bb27bd2d8cf3278fd2ec2a39e5d0330e3e78a5c6d82c1e1d92d57b2f3b5ee2"
 
   url "https://github.com/kmatsushita1012/agent-sushi/releases/download/v#{version}/agent-sushi-#{version}.tar.gz"
   name "Agent Sushi"
